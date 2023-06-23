b12e805d634d137e9601c58c164d3b0a858b926f
Brix Fernando <brixcfernando@gmail.com>
2023-06-23 16:12:38 +0800

    git init

---
+++ BatoBatoPick/BatoBatoPickApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatoBatoPickApp.swift
+//  BatoBatoPick
+//
+//  Created by Brix on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BatoBatoPickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
